3b8c48d0ac6eb7707402de0dbe557a5cd9b44cf0
victory-et <tuduov@gmail.com>
2023-09-10 07:06:00 +0100

    app layout component

--- app/app/Layout.tsx
+++ app/app/Layout.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Sidebar from "@/components/appComponents/Sidebar";
 // creating app layout component
 
 const appLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <Sidebar />
       {children}
-      <div>
-      {/* Main account layout */}
-      </div>
+      <div>{/* Main account layout */}</div>
     </div>
   );
 };
