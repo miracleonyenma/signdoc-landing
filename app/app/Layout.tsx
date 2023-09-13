d5b9962ad89b533a5ea63dd1dfcf65efe736306f
victory-et <tuduov@gmail.com>
2023-09-13 09:05:32 +0100

    layout+app context provider

--- app/app/Layout.tsx
+++ app/app/Layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Sidebar from "@/components/appComponents/Sidebar";
+import { AppContextProvider } from "@/context/AppContext";
+import { useSideBarContext } from "@/context/AppContext";
+
 // creating app layout component
 
 const appLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="app-layout">
-      <Sidebar />
-      {children}
-      <div>{/* Main account layout */}</div>
-    </div>
+    <AppContextProvider>
+      <div className="app-layout">
+        <Sidebar />
+        <div className="app-layout__content">
+          {/* Main account layout */}
+          {children}
+        </div>
+      </div>
+    </AppContextProvider>
   );
 };
 
