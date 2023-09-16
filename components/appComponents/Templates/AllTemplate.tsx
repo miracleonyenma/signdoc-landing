0117a8b1d9f3db5c46608090830870be17a8bb67
victory-et <tuduov@gmail.com>
2023-09-16 11:22:18 +0100

    template card components

---
+++ components/appComponents/Templates/AllTemplate.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AllTemplate = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-cols-max gap-6'>
+        {/* Templates page container component*/}
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+        <div className="h-[333px]  bg-white"></div>
+    </div>
+  )
+}
+
+export default AllTemplate
---
+++ components/appComponents/Templates/TemplateCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TemplateCard = () => {
+  return (
+    <div>
+        {/* Each Individual template card */}
+    </div>
+  )
+}
+
+export default TemplateCard
