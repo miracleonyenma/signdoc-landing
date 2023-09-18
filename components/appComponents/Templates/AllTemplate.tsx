f0f8f27ec5d72fc24b774eb5d5e7bbc507fd003a
victory-et <tuduov@gmail.com>
2023-09-18 12:03:39 +0100

    feat: template page

--- components/appComponents/Templates/AllTemplate.tsx
+++ components/appComponents/Templates/AllTemplate.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import TemplateCard from "./TemplateCard";
 
 const AllTemplate = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-cols-max gap-6'>
-        {/* Templates page container component*/}
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
-        <div className="h-[333px]  bg-white"></div>
+    <div className="template">
+      {/* Templates page container component*/}
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
+      <TemplateCard />
     </div>
-  )
-}
+  );
+};
 
-export default AllTemplate
+export default AllTemplate;
--- components/appComponents/Templates/TemplateCard.tsx
+++ components/appComponents/Templates/TemplateCard.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { FileDashed } from "@phosphor-icons/react";
 
 const TemplateCard = () => {
   return (
     <div>
-        {/* Each Individual template card */}
+      {/* Each Individual template card */}
+      <div className="template-card">
+        <div className="template-card__icon">
+          <FileDashed size={192} weight="thin"/>
+        </div>
+        <div className="template-card__title">Contract Agreement Template</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TemplateCard
+export default TemplateCard;
